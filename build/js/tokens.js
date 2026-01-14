69364b65480bdbdab403d9dfa2882cbf1fe3a5af
pedrosouza44 <pedro_souza44@hotmail.com>
2026-01-14 13:09:18 -0300

    Initial commit

---
+++ build/js/tokens.js
@@ -0,0 +1,13 @@
+/**
+ * Do not edit directly, this file was auto-generated.
+ */
+
+module.exports = {
+  ColorWhite: "#ffffff",
+  ColorBlack: "#000000",
+  ColorGray200: "#c1c1c4",
+  ColorGray500: "#50505c",
+  ColorGray800: "#1c1c1f",
+  ColorGray900: "#121214",
+  ColorPurple500: "#8b5cf6",
+};
